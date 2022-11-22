bb278c69b026d2114b3d8abf46db01e1dc4d7240
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-11-22 11:26:12 +0500

    1-misol

--- cities_dict.py
+++ cities_dict.py
@@ -6,16 +6,6 @@ def cities_dict(cities:list):
     Returns:
         dict: dictionary with keys ordered by city name
     """ 
-towns = [{'name': 'Manchester', 'population': 58241}, 
-         {'name': 'Coventry', 'population': 12435}, 
-         {'name': 'South Windsor', 'population': 25709}]
 
-names       = []
-populations = []
-
-for town in towns:
-    names.append(town["name"])
-    populations.append(town["population"])
-
-print("Name of towns in the city are:", names)
-print("Population of each town in the city are:", populations)
+    return["{}: {}".format(str(i+1), c) for i,c in enumerate(cities)]
+print(cities_dict(["smarqand","navoiy","Jizzax"]))
