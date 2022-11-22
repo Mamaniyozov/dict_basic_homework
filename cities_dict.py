8fc642ee1089842eba15acfa7e58b15723d573a5
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-11-22 15:41:44 +0500

    1-misol 2- urinish

--- cities_dict.py
+++ cities_dict.py
@@ -6,6 +6,6 @@ def cities_dict(cities:list):
     Returns:
         dict: dictionary with keys ordered by city name
     """ 
-
+    
     return["{}: {}".format(str(i+1), c) for i,c in enumerate(cities)]
-print(cities_dict(["smarqand","navoiy","Jizzax"]))
+print(cities_dict([' Samarkand', ' Tashkent', ' Moscow', 'Buenos Aires', 'New York']))
